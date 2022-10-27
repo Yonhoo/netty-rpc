65cd545699505314d6bc66bcf6eb345c7eee237d
Yonhoo <cookies_yon@163.com>
2022-10-27 22:01:01 +0800

    [feat] replace connection rpcResponse use CompletableFuture

--- src/main/java/com/yonhoo/nettyrpc/connection/Connection.java
+++ src/main/java/com/yonhoo/nettyrpc/connection/Connection.java
@@ -5,9 +5,9 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.util.AttributeKey;
-import io.netty.util.concurrent.Promise;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -16,7 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Connection {
     private Channel channel;
-    private final ConcurrentHashMap<Integer, Promise<RpcResponse>> invokePromiseMap =
+    private final ConcurrentHashMap<Integer, CompletableFuture<RpcResponse>> invokeFutureMap =
             new ConcurrentHashMap<>();
     private AtomicInteger referenceCount = new AtomicInteger();
     private AtomicBoolean closed = new AtomicBoolean(false);
@@ -32,16 +32,16 @@ public class Connection {
         return this.channel != null && this.channel.isActive();
     }
 
-    public Promise<RpcResponse> addInvokeFuture(Integer invokeId, Promise<RpcResponse> future) {
-        Promise<RpcResponse> origin = this.invokePromiseMap.putIfAbsent(invokeId, future);
+    public CompletableFuture<RpcResponse> addInvokeFuture(Integer invokeId, CompletableFuture<RpcResponse> future) {
+        CompletableFuture<RpcResponse> origin = this.invokeFutureMap.putIfAbsent(invokeId, future);
         if (origin == null) {
             this.referenceCount.incrementAndGet();
         }
         return origin;
     }
 
-    public Promise<RpcResponse> removeInvokeFuture(Integer invokeId) {
-        Promise<RpcResponse> result = this.invokePromiseMap.remove(invokeId);
+    public CompletableFuture<RpcResponse> removeInvokeFuture(Integer invokeId) {
+        CompletableFuture<RpcResponse> result = this.invokeFutureMap.remove(invokeId);
         if (result != null) {
             this.referenceCount.decrementAndGet();
         }
@@ -72,13 +72,13 @@ public class Connection {
     }
 
     private void onClose() {
-        Iterator<Map.Entry<Integer, Promise<RpcResponse>>> iter = invokePromiseMap.entrySet().iterator();
+        Iterator<Map.Entry<Integer, CompletableFuture<RpcResponse>>> iter = invokeFutureMap.entrySet().iterator();
         while (iter.hasNext()) {
-            Map.Entry<Integer, Promise<RpcResponse>> entry = iter.next();
+            Map.Entry<Integer, CompletableFuture<RpcResponse>> entry = iter.next();
             iter.remove();
-            Promise<RpcResponse> future = entry.getValue();
+            CompletableFuture<RpcResponse> future = entry.getValue();
             if (future != null) {
-                future.setFailure(new Throwable("connection closed"));
+                future.completeExceptionally(new Throwable("connection closed"));
             }
         }
     }
