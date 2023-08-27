e076c0883c13fb2a29a7a38772b9ad7ab81ad3fd
YonHoo <cookies_yon@163.com>
2023-08-27 09:08:24 +0800

    [feat]: change DirtiesContext

--- src/test/java/com/yonhoo/nettyrpc/server_base/BaseIntegrationTest.java
+++ src/test/java/com/yonhoo/nettyrpc/server_base/BaseIntegrationTest.java
@@ -2,12 +2,15 @@ package com.yonhoo.nettyrpc.server_base;
 
 import com.yonhoo.nettyrpc.registry.base.BaseZkTest;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(classes = NettyApplicationTest.class)
 public abstract class BaseIntegrationTest extends BaseZkTest {
 }
